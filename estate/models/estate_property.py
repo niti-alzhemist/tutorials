d7b7144786feb2a179f700105b4fdd3a47da2601
niti-jira <niti@alzhemist.com>
2024-05-23 16:38:18 +0700

    feat(chapter6): basic view

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,18 +5,34 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Property"
 
+    def _default_availability_date(self):
+        return fields.Date.add(fields.Date.today(), months=3)
+
+    active = fields.Boolean(default=True)
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=_default_availability_date)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_areas = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         [("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")],
+    )
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        required=True,
+        copy=False,
+        default="new",
     )
