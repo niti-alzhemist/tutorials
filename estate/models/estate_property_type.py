79acb2bb1dc85d64d90e2ae057ea8f0a06ba879c
niti-jira <niti@alzhemist.com>
2024-05-28 14:38:04 +0700

    chapter(17): more buttons

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, _
 
 
 class EstatePropertyType(models.Model):
@@ -9,3 +9,24 @@ class EstatePropertyType(models.Model):
     name = fields.Char(required=True)
     property_ids = fields.One2many("estate.property", "property_type_id")
     sequence = fields.Integer(default=1)
+    offer_ids = fields.One2many('estate.property.offer', 'property_type_id', )
+    offer_count = fields.Integer(compute="_compute_offer_count")
+
+    # compute methods
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.mapped('offer_ids'))
+
+    # actions
+    def action_view_offers(self):
+        return {
+            "name": _("Related Offers"),
+            "type": 'ir.actions.act_window',
+            "view_mode": "tree,form",
+            "res_model": 'estate.property.offer',
+            "target": 'current',
+            "domain": [('property_type_id', '=', self.id)],
+            "context": {
+                "default_property_type_id": self.id
+            }
+        }
