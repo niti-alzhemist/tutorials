2f3fd1bd3e1489b429fe8947e4a1ecbf903d5b80
niti-jira <niti@alzhemist.com>
2024-05-28 17:21:22 +0700

    chapter(19): view inheritance

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -4,4 +4,5 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    property_ids = fields.One2many('estate.property', 'seller_id', domain=[("state", "in" "('new', 'offer_received')")])
+    property_ids = fields.One2many('estate.property', 'seller_id',
+                                   domain=[("state", "in", ['new', 'offer_received'])])
